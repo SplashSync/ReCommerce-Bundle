9d8ce332b960ddcaab913663e7eb06ac0a25a43d
BadPixxel <eshop.bpaquier@gmail.com>
2021-07-02 13:52:00 +0200

    FIX: Phpcore 1.7.1

--- tests/Controller/S01WebHookTest.php
+++ tests/Controller/S01WebHookTest.php
@@ -191,7 +191,7 @@ class S01WebHookTest extends TestCase
 
         //====================================================================//
         // Verify Response
-        $this->assertIsLastCommited($action, $objectType, $objectId);
+        $this->assertIsLastCommitted($action, $objectType, $objectId);
     }
 
     /**
